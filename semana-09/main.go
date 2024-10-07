392ee5e13009dfbeca5d5bd37cd52a42a3042735
Rafael Techio <110608373+RafaelTechio@users.noreply.github.com>
2024-10-07 00:39:31 -0300

    feat: comments

--- semana-09/main.go
+++ semana-09/main.go
@@ -1,3 +1,8 @@
+/*
+Os testes de carga se relacionam com o tdd em um nível superior de maturidade, visto que seu objetivo é testar a qualidade do software, e não seu funcionamento em si. 
+Dessa forma, o foco é a testagem de performance, e para isso, diversas iterações do TDD já devem ter ocorrido.
+Contudo, após uma base funcional estabelicida, é totalmente possível utilizar de testes de carga no ciclo do tdd, tendo diversos benefícios. 
+*/
 package main
 
 import (
